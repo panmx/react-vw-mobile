5c55513c69cc992a65cf8957f3206a934870a51d
panmx <1983646964@qq.com>
2020-07-03 15:08:49 +0800

    修改路由配置

--- src/index.js
+++ src/index.js
@@ -7,12 +7,47 @@ import '@/scss/reset.scss';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import configureStore from '@/store';
+import {Redirect, Route, Switch} from "react-router";
+import {Router} from "react-router-dom";
+import {history,routes} from '@/router';
 const store = configureStore();
+
+function getRouterByRoutes(routes){
+    const renderedRoutesList = [];
+    const renderRoutes = (routes,parentPath)=>{
+        Array.isArray(routes)&&routes.forEach((route)=>{
+            const {path,redirect,children,layout,component} = route;
+            if(redirect){
+                renderedRoutesList.push(<Redirect key={`${parentPath}${path}`} exact from={path} to={`${parentPath}${redirect}`}/>)
+            }
+            if(component){
+                renderedRoutesList.push(
+                    layout?<Route
+                            key={`${parentPath}${path}`}
+                            exact path={`${parentPath}${path}`}
+                            render={(props)=>React.createElement(layout,props,React.createElement(component,props))} />:
+                        <Route
+                            key={`${parentPath}${path}`}
+                            exact
+                            path={`${parentPath}${path}`}
+                            component={component}/>)
+            }
+            if(Array.isArray(children)&&children.length>0){
+                renderRoutes(children,path)
+            }
+        });
+    }
+    renderRoutes(routes,'')
+    return renderedRoutesList;
+}
 render(
     <Provider store={store}>
-        <App/>
+        <Router history={history}>
+            <Switch>
+                {getRouterByRoutes(routes)}
+            </Switch>
+        </Router>
     </Provider>,
     document.getElementById("root")
 );
